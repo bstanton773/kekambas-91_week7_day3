529566f13b7cf7bd27d55fc246394e30b3527fee
Brian <bstanton773@gmail.com>
2022-06-15 15:32:43 -0500

    Multiple promises, thens, catch

--- js/promises.js
+++ js/promises.js
@@ -22,7 +22,33 @@ function downloadSong(songName){
     })
 }
 
+// let mySong = downloadSong('Mrs. Robinson');
+// console.log(mySong);
+// mySong.then(song => console.log(song.toUpperCase()));
 
 downloadSong('Yesterday').then(s => console.log(`We are now playing ${s}`))
 
 downloadSong('ABC').then(s => console.log(`We are now playing ${s}`)).catch(e => console.warn(e))
+
+
+function playSong(songFile){
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(`${songFile} is playing...`)
+        }, 2000)
+    })
+}
+
+
+
+
+downloadSong('ABC')
+.then((s) => {
+    console.log(`${s} has downloaded`)
+    return playSong(s)
+})
+.then(message => console.log(message))
+.catch(err => console.warn(err))
+
+
+
